c1ed84bf2cf40e00d276f1a320663c609f25858e
Sergio Morales <chemoral87@hotmail.com>
2021-11-30 01:37:20 -0600

    tet

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
   'paths' => ['api/*'],
   'allowed_methods' => ['*'],
 
-  'allowed_origins' => ['http://localhost:*', 'http://localhost:3000', 'https://dev.d2ax9tya2rkwv4.amplifyapp.com', 'https://rcdesarrolladora.cloudns.ph', '*'],
+  'allowed_origins' => ['http://localhost:*', 'http://localhost:3000', 'https://dev.d2ax9tya2rkwv4.amplifyapp.com'],
 
   'allowed_origins_patterns' => [],
 
