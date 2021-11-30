efa8e87e85a399c785d4aa29a3dc0779e99652e5
Sergio Morales <chemoral87@hotmail.com>
2021-11-30 02:40:37 -0600

    test

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,20 @@
 <?php
 
 return [
+  'paths' => ['api/*'],
+  'allowed_methods' => ['*'],
 
+  //'allowed_origins' => ['http://localhost:*', 'http://localhost:3000', '*'],
+  'allowed_origins' => ['http://localhost:*', 'http://localhost:3000', 'https://dev.d2ax9tya2rkwv4.amplifyapp.com'],
+  'allowed_origins_patterns' => [],
+
+  'allowed_headers' => ['*'],
+
+  'exposed_headers' => false,
+
+  'max_age' => 0,
+
+  'supports_credentials' => false,
   /*
   |--------------------------------------------------------------------------
   | Laravel CORS Options
@@ -21,40 +34,40 @@ return [
    * You can enable CORS for 1 or multiple paths.
    * Example: ['api/*']
    */
-  'paths' => ['api/*', 'auth/*'],
+  // 'paths' => ['api/*'],
 
-  /*
-   * Matches the request method. `['*']` allows all methods.
-   */
-  'allowed_methods' => ['*'],
+  // /*
+  //  * Matches the request method. `['*']` allows all methods.
+  //  */
+  // 'allowed_methods' => ['*'],
 
-  /*
-   * Matches the request origin. `['*']` allows all origins. Wildcards can be used, eg `*.mydomain.com`
-   */
-  'allowed_origins' => ['*'],
+  // /*
+  //  * Matches the request origin. `['*']` allows all origins. Wildcards can be used, eg `*.mydomain.com`
+  //  */
+  // 'allowed_origins' => ['http://localhost:*', 'http://localhost:3000'],
 
-  /*
-   * Patterns that can be used with `preg_match` to match the origin.
-   */
-  'allowed_origins_patterns' => [],
+  // /*
+  //  * Patterns that can be used with `preg_match` to match the origin.
+  //  */
+  // 'allowed_origins_patterns' => [],
 
-  /*
-   * Sets the Access-Control-Allow-Headers response header. `['*']` allows all headers.
-   */
-  'allowed_headers' => ['content-type', 'accept', 'x-custom-header', 'Access-Control-Allow-Origin'],
+  // /*
+  //  * Sets the Access-Control-Allow-Headers response header. `['*']` allows all headers.
+  //  */
+  // 'allowed_headers' => ['content-type', 'accept', 'x-custom-header', 'Access-Control-Allow-Origin'],
 
-  /*
-   * Sets the Access-Control-Expose-Headers response header with these headers.
-   */
-  // 'exposed_headers' => ['*'],
-  'exposed_headers' => ['x-custom-response-header'],
-  /*
-   * Sets the Access-Control-Max-Age response header when > 0.
-   */
-  'max_age' => 0,
+  // /*
+  //  * Sets the Access-Control-Expose-Headers response header with these headers.
+  //  */
+  // // 'exposed_headers' => ['*'],
+  // 'exposed_headers' => ['x-custom-response-header'],
+  // /*
+  //  * Sets the Access-Control-Max-Age response header when > 0.
+  //  */
+  // 'max_age' => 0,
 
-  /*
-   * Sets the Access-Control-Allow-Credentials header.
-   */
-  'supports_credentials' => false,
+  // /*
+  //  * Sets the Access-Control-Allow-Credentials header.
+  //  */
+  // 'supports_credentials' => false,
 ];
