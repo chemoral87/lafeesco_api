a32ec6e3f4c1f7b039afe938f56497682207e23d
Sergio Morales <chemoral87@hotmail.com>
2021-11-30 02:04:45 -0600

    cors3

--- config/cors.php
+++ config/cors.php
@@ -15,18 +15,16 @@ return [
   |
    */
 
-  //'paths' => ['api/*'],
   'paths' => ['api/*'],
   'allowed_methods' => ['*'],
 
-  //'allowed_origins' => ['http://localhost:*', 'http://localhost:3000', '*.amplifyapp.com'],
   'allowed_origins' => ['*'],
 
   'allowed_origins_patterns' => ['*'],
 
   'allowed_headers' => ['*'],
 
-  'exposed_headers' => false,
+  'exposed_headers' => [],
 
   'max_age' => 0,
 
