96fa3da1e8c38e78cbac4caa993eb983bb785ed6
Sergio Morales <chemoral87@hotmail.com>
2022-05-04 22:42:14 -0500

    required

--- config/database.php
+++ config/database.php
@@ -58,9 +58,10 @@ return [
       'prefix_indexes' => true,
       'strict' => true,
       'engine' => null,
-      'sslmode' => env('DB_SSLMODE', 'prefer'),
+
       'options' => extension_loaded('pdo_mysql') ? array_filter([
         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        'sslmode' => env('MYSQL_ATTR_SSL_MODE', 'prefer'),
       ]) : [],
     ],
 
