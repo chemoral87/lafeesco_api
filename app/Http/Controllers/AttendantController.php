0d66a46d2db9dcf7f1961c5af5f257d7111aef74
Sergio Morales <chemoral87@hotmail.com>
2022-10-28 01:09:30 -0500

    attendant

--- app/Http/Controllers/AttendantController.php
+++ app/Http/Controllers/AttendantController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\DataSetResource;
 use App\Models\Attendant;
+use Illuminate\Http\Request;
 
 class AttendantController extends Controller {
   const PATH_S3 = "attendant/";
@@ -15,8 +16,22 @@ class AttendantController extends Controller {
   }
 
   public function create(Request $request) {
-    $house_faith = FaithHouse::create($request->all());
-    return ['success' => __('messa.house_faith_create')];
+    $photo = "";
+    if ($request->has('photo')) {
+      $photo = saveS3Blob($blob, self::PATH_S3);
+    }
+
+    $attendant = Attendant::create([
+      'name' => $request->get('name'),
+      'paternal_surname' => $request->get('paternal_surname'),
+      'maternal_surname' => $request->get('maternal_surname'),
+      'cellphone' => $request->get('cellphone'),
+      'photo' => $photo,
+      'email' => $request->get('email'),
+      'birthdate' => $request->get('birthdate'),
+    ]);
+
+    return ['success' => __('messa.attendant_create')];
   }
 
   public function update(Request $request, $id) {
