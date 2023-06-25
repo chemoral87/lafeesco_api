2e22bc0f73b9dc181cd37a6a39ca66eb9b7ae953
Sergio Morales <chemoral87@hotmail.com>
2023-06-25 17:18:51 -0600

    assing method church service attendant

--- app/Http/Controllers/ChurchServiceAttendantController.php
+++ app/Http/Controllers/ChurchServiceAttendantController.php
@@ -8,13 +8,10 @@ use Illuminate\Http\Request;
 class ChurchServiceAttendantController extends Controller {
   public function update(Request $request) {
     $attendant_ids = $request->get("attendant_ids");
-    // VALIDATION
-
-    // INSERTION
     $church_service_id = $request->get("church_service_id");
     $ministry_id = $request->get("ministry_id");
+    // VALIDATION
 
-    // DELETE REMOVED ATTENDANT
     $attendants = ChurchServiceAttendant::where('church_service_id', $church_service_id)
       ->where('ministry_id', $ministry_id)
     // ->whereNotIn("attendant_id", $attendant_ids)
@@ -23,41 +20,36 @@ class ChurchServiceAttendantController extends Controller {
     $attendants_assined = $attendants->pluck("attendant_id")->all();
     $attendants_to_insert = array_diff($attendant_ids, $attendants_assined);
     $attendants_to_delete = array_diff($attendants_assined, $attendant_ids);
+    $attendants_to_update = array_diff($attendants_assined, $attendants_to_insert);
 
-// DELETE church services attendant
-    foreach ($attendants_to_delete as $key => $attendant_id) {
-      $attendant = ChurchServiceAttendant::where("attendant_id", $attendant_id)
-        ->where("church_service_id", $church_service_id)
-        ->where("ministry_id", $ministry_id)
-        ->first();
-      $attendant->delete();
-    }
-
-// INSERT church services attendant
+    // INSERT church services attendant
+    $newMembers = [];
     foreach ($attendants_to_insert as $key => $attendant_id) {
-      $member = ChurchServiceAttendant::create([
+      $newMembers[] = [
         'church_service_id' => $church_service_id,
         'ministry_id' => $ministry_id,
         'attendant_id' => $attendant_id,
-      ]);
+        "seq" => array_search($attendant_id, $attendant_ids),
+      ];
     }
+    ChurchServiceAttendant::insert($newMembers);
+
+    // DELETE church services attendant
+    ChurchServiceAttendant::whereNotIn("attendant_id", $attendant_ids)
+      ->where("church_service_id", $church_service_id)
+      ->where("ministry_id", $ministry_id)
+      ->delete();
 
     //UPDATE seq church services attendant
-    foreach ($attendants_to_insert as $key => $attendant_id) {
+    foreach ($attendants_to_update as $key => $attendant_id) {
       ChurchServiceAttendant::where("attendant_id", $attendant_id)
         ->where("ministry_id", $ministry_id)
         ->where("church_service_id", $church_service_id)
-        ->update(["seq" => $key]);
-
+        ->update(["seq" => array_search($attendant_id, $attendant_ids)]);
     }
 
     return [
       'success' => __('messa.church_service_attendant_create'),
-      'attendants' => $attendants,
-      'attendant_ids' => is_array($attendant_ids),
-      'attendants_assined' => is_array($attendants_assined),
-      'attendants_to_delete' => $attendants_to_delete,
-      'attendants_to_insert' => $attendants_to_insert,
     ];
   }
 }
