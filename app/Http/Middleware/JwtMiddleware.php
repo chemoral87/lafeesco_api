d74a6739560aa82f93f52bce375ce046b271adb8
Sergio Morales <chemoral87@hotmail.com>
2023-07-03 11:52:13 -0600

    handle 401 errors token

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -21,11 +21,11 @@ class JwtMiddleware {
       $user = JWTAuth::parseToken()->authenticate();
     } catch (Exception $e) {
       if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException ) {
-        return response()->json(['status' => 'Token is Invalid'], 401);
+        return response()->json(['errors' => ['status' => 'Token is Invalid']], 401);
       } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException ) {
-        return response()->json(['status' => 'Token is Expired'], 401);
+        return response()->json(['errors' => ['status' => 'Token is Expired']], 401);
       } else {
-        return response()->json(['status' => 'Authorization Token not found'], 401);
+        return response()->json(['errors' => ['status' => 'Authorization Token not found']], 401);
       }
     }
     return $next($request);
