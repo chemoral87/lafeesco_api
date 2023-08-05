10c277ac2bc250d0d7c8d290fb37f9cc10e3eabb
Sergio Morales <chemoral87@hotmail.com>
2023-08-05 08:53:28 -0600

    Sky kids

---
+++ database/migrations/2023_08_03_010500_create_sky_parents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::create('sky_parents', function (Blueprint $table) {
+      $table->id();
+      $table->string("name");
+      $table->string("paternal_surname");
+      $table->string("maternal_surname")->nullable();
+      $table->string("cellphone", 13)->nullable();
+      $table->string("photo")->nullable();
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::dropIfExists('sky_parents');
+  }
+};
---
+++ database/migrations/2023_08_03_011927_create_sky_entrances_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::create('sky_entrances', function (Blueprint $table) {
+      $table->id();
+      $table->bigInteger('sky_kid_id')->unsigned();
+      $table->string('room');
+      $table->datetime('exits')->nullable();
+      $table->string('ticket');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::dropIfExists('sky_entrances');
+  }
+};
